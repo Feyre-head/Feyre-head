72a2bb20cf610fc26042b53de89302c0d7d4f323
Feyre <inaraoenning@gmail.com>
2024-10-15 15:32:39 -0400

    Subindo Website pronto

---
+++ script.js
@@ -0,0 +1,5 @@
+// Selecionar o elemento de áudio
+const audioElement = document.getElementById('music-player');
+
+// Definir o volume inicial (por exemplo, 50%)
+audioElement.volume = 0.05; // Volume vai de 0.0 a 1.0
